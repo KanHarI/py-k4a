71a18f30e646ee91a5423c499e57fe32c2562043
Itaykh <knaan.harpaz@gmail.com>
2020-01-08 16:42:20 +0200

    WIP

--- transformation.c
+++ transformation.c
@@ -4,6 +4,8 @@
 
 #include "types.h"
 
+#include <stdio.h>
+
 PyObject* transformation_depth_image_to_color_camera(PyObject* self, PyObject* args)
 {
 	DeviceObject *obj;
@@ -63,7 +65,11 @@ PyObject* calibration_2d_to_3d(PyObject* self, PyObject* args)
 	k4a_float2_t pixel_loc;
 	float depth;
 
+    printf("Line 68!");
+
 	PyArg_ParseTuple("OOfff", &obj, &config, &pixel_loc.xy.x, &pixel_loc.xy.y, &depth);
+
+    printf("Line 72!");
 
 	k4a_calibration_t calibration;
 
@@ -74,15 +80,19 @@ PyObject* calibration_2d_to_3d(PyObject* self, PyObject* args)
         &calibration
     );
 
+    printf("Line 83!");
+
     if (result != K4A_RESULT_SUCCEEDED)
     {
         return Py_None;
     }
 
+    printf("Line 90!");
+
     k4a_float3_t _3d_loc;
     int is_valid;
 
-    // K4A_CALIBRATION_TYPE_COLOR
+    printf("Line 97!");
 
     result = k4a_calibration_2d_to_3d(
     	&calibration,
@@ -94,10 +104,14 @@ PyObject* calibration_2d_to_3d(PyObject* self, PyObject* args)
     	&is_valid
     );
 
+    printf("Line 107!");
+
     if (result != K4A_RESULT_SUCCEEDED || !is_valid)
     {
     	return Py_None;
     }
+
+    printf("Line 114!");
 
 	return Py_BuildValue("fff", _3d_loc.xyz.x, _3d_loc.xyz.y, _3d_loc.xyz.z);
 }
